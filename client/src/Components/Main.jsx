9c9ca802f20e871477a761e7cfeae6c64d453d3d
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-12-11 13:49:56 +0100

    Update Main.jsx

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -10,7 +10,7 @@ const gardeData = [
     { value: 'nuit', label: 'Nuit' },
 ]
 
-const URL = 'https://frightened-fez-cow.cyclic.app:9000';
+const URL = 'https://pharma-tldq.onrender.com';
 
 const defaultCity = { value: "city", label: "Select City" };
 const defaultZone = { value: "zone", label: "Select City" };
